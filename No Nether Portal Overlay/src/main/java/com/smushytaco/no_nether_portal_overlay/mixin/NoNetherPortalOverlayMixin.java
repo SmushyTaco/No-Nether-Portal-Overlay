c46848625c2d89b30e8dbaab3a8c167bdfe760da
Nikan Radan <github.com@alias.nikanradan.com>
2024-05-29 19:56:37 -0700

    1.20.6

--- No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
+++ No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
@@ -9,6 +9,5 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(ClientPlayerEntity.class)
 public abstract class NoNetherPortalOverlayMixin {
     @WrapOperation(method = "updateNausea", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/ClientPlayerEntity;inNetherPortal:Z", opcode = Opcodes.GETFIELD))
-    @SuppressWarnings("unused")
     private boolean hookUpdateNausea(ClientPlayerEntity instance, Operation<Boolean> original) { return !NoNetherPortalOverlay.INSTANCE.getConfig().isEnabled() && original.call(instance); }
 }
--- No Nether Portal Overlay/src/main/kotlin/com/smushytaco/no_nether_portal_overlay/NoNetherPortalOverlayPreLaunch.kt
+++
@@ -1,5 +0,0 @@
-package com.smushytaco.no_nether_portal_overlay
-import com.llamalad7.mixinextras.MixinExtrasBootstrap
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-@Suppress("UNUSED")
-object NoNetherPortalOverlayPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { MixinExtrasBootstrap.init() } }
