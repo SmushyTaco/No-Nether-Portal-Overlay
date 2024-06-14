70609b9558b96eae9ba16130625a9983761c10a4
Nikan Radan <github.com@alias.nikanradan.com>
2024-06-13 19:29:24 -0700

    1.21

--- No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
+++ No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
@@ -3,11 +3,11 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.no_nether_portal_overlay.NoNetherPortalOverlay;
 import net.minecraft.client.network.ClientPlayerEntity;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.world.dimension.PortalManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(ClientPlayerEntity.class)
 public abstract class NoNetherPortalOverlayMixin {
-    @WrapOperation(method = "updateNausea", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/ClientPlayerEntity;inNetherPortal:Z", opcode = Opcodes.GETFIELD))
-    private boolean hookUpdateNausea(ClientPlayerEntity instance, Operation<Boolean> original) { return !NoNetherPortalOverlay.INSTANCE.getConfig().isEnabled() && original.call(instance); }
+    @WrapOperation(method = "tickNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/PortalManager;isInPortal()Z"))
+    private boolean hookTickNausea(PortalManager instance, Operation<Boolean> original) { return !NoNetherPortalOverlay.INSTANCE.getConfig().isEnabled() && original.call(instance); }
 }
