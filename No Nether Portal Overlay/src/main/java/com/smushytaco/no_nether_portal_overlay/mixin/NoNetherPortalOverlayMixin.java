69fc46e1351b26c87b3dcb1be94d6c5566bd3356
Nikan Radan <github.com@alias.nikanradan.com>
2025-11-28 21:57:55 -0800

    Official Mappings

--- No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
+++ No Nether Portal Overlay/src/main/java/com/smushytaco/no_nether_portal_overlay/mixin/NoNetherPortalOverlayMixin.java
@@ -2,12 +2,12 @@ package com.smushytaco.no_nether_portal_overlay.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.no_nether_portal_overlay.NoNetherPortalOverlay;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.world.dimension.PortalManager;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.PortalProcessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-@Mixin(ClientPlayerEntity.class)
+@Mixin(LocalPlayer.class)
 public abstract class NoNetherPortalOverlayMixin {
-    @WrapOperation(method = "tickNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/PortalManager;isInPortal()Z"))
-    private boolean hookTickNausea(PortalManager instance, Operation<Boolean> original) { return !NoNetherPortalOverlay.INSTANCE.getConfig().isEnabled() && original.call(instance); }
+    @WrapOperation(method = "handlePortalTransitionEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/PortalProcessor;isInsidePortalThisTick()Z"))
+    private boolean hookTickNausea(PortalProcessor instance, Operation<Boolean> original) { return !NoNetherPortalOverlay.INSTANCE.getConfig().isEnabled() && original.call(instance); }
 }
